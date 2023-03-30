25b5fea0efb2c6dcdab092c449d60fd159486797
Tom Deseyn <tom.deseyn@gmail.com>
2023-03-30 12:09:47 +0200

    login: print the AuthenticationException.Message on failure. (#43)

--- src/dotnet-shift/OpenShift/OpenShiftClientException.cs
+++ src/dotnet-shift/OpenShift/OpenShiftClientException.cs
@@ -1,13 +1,13 @@
+using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Authentication;
 
 namespace OpenShift;
 
-
-
-class OpenShiftClientException : System.Exception
+class OpenShiftClientException : Exception
 {
-    internal OpenShiftClientException(string host, string message, OpenShiftClientExceptionCause cause, HttpStatusCode? httpStatusCode, string? responseText, System.Exception? innerException) :
+    internal OpenShiftClientException(string host, string message, OpenShiftClientExceptionCause cause, HttpStatusCode? httpStatusCode, string? responseText, Exception? innerException) :
         base(message, innerException)
     {
         Host = host;
@@ -22,7 +22,25 @@ class OpenShiftClientException : System.Exception
 
     public HttpStatusCode? HttpStatusCode { get; }
 
-    public SocketError? SocketError => (InnerException?.InnerException as SocketException)?.SocketErrorCode;
+    public SocketError? SocketError => SocketException?.SocketErrorCode;
 
     public string? ResponseText { get; }
+
+    public SocketException? SocketException => FindInnerException<SocketException>();
+
+    public AuthenticationException? AuthenticationException => FindInnerException<AuthenticationException>();
+
+    private T? FindInnerException<T>() where T: Exception
+    {
+        Exception? ex = InnerException;
+        while (ex is not null)
+        {
+            if (ex is T value)
+            {
+                return value;
+            }
+            ex = ex.InnerException;
+        }
+        return null;
+    }
 }
