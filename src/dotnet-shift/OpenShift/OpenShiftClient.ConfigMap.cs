87663aa66ddf58c024c395166dacfc7444701b21
Tom Deseyn <tom.deseyn@gmail.com>
2023-03-30 12:46:44 +0200

    deploy: also update the ConfigMap.

--- src/dotnet-shift/OpenShift/OpenShiftClient.ConfigMap.cs
+++ src/dotnet-shift/OpenShift/OpenShiftClient.ConfigMap.cs
@@ -15,6 +15,9 @@ partial class OpenShiftClient : IOpenShiftClient
     public Task DeleteConfigMapAsync(string name, CancellationToken cancellationToken)
         => DeleteCoreV1NamespacedConfigMapAsync(name, Namespace, cancellationToken: cancellationToken);
 
+    public Task<ConfigMap> PatchConfigMapAsync(ConfigMap configMap, CancellationToken cancellationToken)
+        => PatchCoreV1NamespacedConfigMapAsync(configMap, configMap.Metadata.Name, Namespace, cancellationToken: cancellationToken);
+
     private async Task<ConfigMap?> ReadCoreV1NamespacedConfigMapAsync(string name, string @namespace, string? pretty = null, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
     {
         if (name == null)
@@ -412,6 +415,118 @@ partial class OpenShiftClient : IOpenShiftClient
                     if (status_ == 202)
                     {
                         var objectResponse_ = await ReadObjectResponseAsync<Status>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                        if (objectResponse_.Object == null)
+                        {
+                            throw CreateApiException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                        }
+                        return objectResponse_.Object;
+                    }
+                    else
+                    if (status_ == 401)
+                    {
+                        string responseText_ = (response_.Content == null) ? string.Empty : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                        throw CreateApiException("Unauthorized", status_, responseText_, headers_, null);
+                    }
+                    else
+                    {
+                        var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                        throw CreateApiException("The HTTP status code of the response was not expected (" + status_ + ").", status_, responseData_, headers_, null);
+                    }
+                }
+                finally
+                {
+                    if (disposeResponse_)
+                        response_.Dispose();
+                }
+            }
+        }
+        finally
+        {
+            if (disposeClient_)
+                client_.Dispose();
+        }
+    }
+
+    private async Task<ConfigMap> PatchCoreV1NamespacedConfigMapAsync(ConfigMap body, string name, string @namespace, string? dryRun = null, string? fieldManager = null, bool? force = null, string? pretty = null, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
+    {
+        if (name == null)
+            throw new System.ArgumentNullException("name");
+
+        if (@namespace == null)
+            throw new System.ArgumentNullException("@namespace");
+
+        if (body == null)
+            throw new System.ArgumentNullException("body");
+
+        var urlBuilder_ = new System.Text.StringBuilder();
+        urlBuilder_.Append(BaseUrl != null ? BaseUrl.TrimEnd('/') : "").Append("/api/v1/namespaces/{namespace}/configmaps/{name}?");
+        urlBuilder_.Replace("{name}", System.Uri.EscapeDataString(ConvertToString(name, System.Globalization.CultureInfo.InvariantCulture)));
+        urlBuilder_.Replace("{namespace}", System.Uri.EscapeDataString(ConvertToString(@namespace, System.Globalization.CultureInfo.InvariantCulture)));
+        if (dryRun != null)
+        {
+            urlBuilder_.Append(System.Uri.EscapeDataString("dryRun") + "=").Append(System.Uri.EscapeDataString(ConvertToString(dryRun, System.Globalization.CultureInfo.InvariantCulture))).Append("&");
+        }
+        if (fieldManager != null)
+        {
+            urlBuilder_.Append(System.Uri.EscapeDataString("fieldManager") + "=").Append(System.Uri.EscapeDataString(ConvertToString(fieldManager, System.Globalization.CultureInfo.InvariantCulture))).Append("&");
+        }
+        if (force != null)
+        {
+            urlBuilder_.Append(System.Uri.EscapeDataString("force") + "=").Append(System.Uri.EscapeDataString(ConvertToString(force, System.Globalization.CultureInfo.InvariantCulture))).Append("&");
+        }
+        if (pretty != null)
+        {
+            urlBuilder_.Append(System.Uri.EscapeDataString("pretty") + "=").Append(System.Uri.EscapeDataString(ConvertToString(pretty, System.Globalization.CultureInfo.InvariantCulture))).Append("&");
+        }
+        urlBuilder_.Length--;
+
+        var client_ = _httpClient;
+        var disposeClient_ = false;
+        try
+        {
+            using (var request_ = new System.Net.Http.HttpRequestMessage())
+            {
+                var json_ = Newtonsoft.Json.JsonConvert.SerializeObject(body, _settings.Value);
+                var content_ = new System.Net.Http.StringContent(json_);
+                content_.Headers.ContentType = System.Net.Http.Headers.MediaTypeHeaderValue.Parse("application/strategic-merge-patch+json");
+                request_.Content = content_;
+                request_.Method = new System.Net.Http.HttpMethod("PATCH");
+                request_.Headers.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("application/json"));
+
+                PrepareRequest(client_, request_, urlBuilder_);
+
+                var url_ = urlBuilder_.ToString();
+                request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+
+                PrepareRequest(client_, request_, url_);
+
+                var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                var disposeResponse_ = true;
+                try
+                {
+                    var headers_ = System.Linq.Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+                    if (response_.Content != null && response_.Content.Headers != null)
+                    {
+                        foreach (var item_ in response_.Content.Headers)
+                            headers_[item_.Key] = item_.Value;
+                    }
+
+                    ProcessResponse(client_, response_);
+
+                    var status_ = (int)response_.StatusCode;
+                    if (status_ == 200)
+                    {
+                        var objectResponse_ = await ReadObjectResponseAsync<ConfigMap>(response_, headers_, cancellationToken).ConfigureAwait(false);
+                        if (objectResponse_.Object == null)
+                        {
+                            throw CreateApiException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
+                        }
+                        return objectResponse_.Object;
+                    }
+                    else
+                    if (status_ == 201)
+                    {
+                        var objectResponse_ = await ReadObjectResponseAsync<ConfigMap>(response_, headers_, cancellationToken).ConfigureAwait(false);
                         if (objectResponse_.Object == null)
                         {
                             throw CreateApiException("Response was null which was not expected.", status_, objectResponse_.Text, headers_, null);
