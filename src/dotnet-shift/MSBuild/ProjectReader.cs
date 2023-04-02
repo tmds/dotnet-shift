88537fba456eda779bafa205ab477807c929cdbf
Tom Deseyn <tom.deseyn@gmail.com>
2023-04-02 07:06:20 +0200

    deploy: add envvars from proj ContainerEnvironmentVariable. (#53)

--- src/dotnet-shift/MSBuild/ProjectReader.cs
+++ src/dotnet-shift/MSBuild/ProjectReader.cs
@@ -1,6 +1,7 @@
 namespace MSBuild;
 
 using System.Linq;
+using Microsoft.Build.Evaluation;
 
 sealed class ProjectReader : IProjectReader
 {
@@ -17,10 +18,22 @@ sealed class ProjectReader : IProjectReader
 
         string? assemblyName = GetProperty(project, "AssemblyName");
 
+        ProjectItem[] environmentVariableItems = GetItems(project, "ContainerEnvironmentVariable");
+        Dictionary<string,string> environmentVariables = new();
+        foreach (var envvarItem in environmentVariableItems)
+        {
+            string? value = GetMetadata(envvarItem, "Value");
+            if (value is not null)
+            {
+                environmentVariables[envvarItem.EvaluatedInclude] = value;
+            }
+        }
+
         var info = new ProjectInformation()
         {
             DotnetVersion = dotnetVersion,
-            AssemblyName = assemblyName
+            AssemblyName = assemblyName,
+            ContainerEnvironmentVariables = environmentVariables
         };
 
         project.ProjectCollection.UnloadProject(project);
@@ -30,6 +43,16 @@ sealed class ProjectReader : IProjectReader
         static string? GetProperty(Microsoft.Build.Evaluation.Project project, string name)
         {
             return project.AllEvaluatedProperties.FirstOrDefault(prop => prop.Name == name)?.EvaluatedValue;
+        }
+
+        static ProjectItem[] GetItems(Project project, string name)
+        {
+            return project.AllEvaluatedItems.Where(item => item.ItemType == name).ToArray();
+        }
+
+        static string? GetMetadata(ProjectItem item, string name)
+        {
+            return item.DirectMetadata.FirstOrDefault(m => m.Name == name)?.EvaluatedValue;
         }
     }
 }
--- src/dotnet-shift/ProjectInformation.cs
+++ src/dotnet-shift/ProjectInformation.cs
@@ -1,5 +1,6 @@
 sealed record ProjectInformation
 {
-    public string? DotnetVersion { get; set; }
-    public string? AssemblyName { get; set; }
+    public string? DotnetVersion { get; init; }
+    public string? AssemblyName { get; init; }
+    public required Dictionary<string, string> ContainerEnvironmentVariables { get; init; }
 }
